7d6ac162412db5263f62bfe73a7b6cd6a610bcc2
afren23 <afriin474@gmail.com>
2024-11-02 14:55:33 +0600

    cors

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 5000;
 const axios = require('axios');
 
 app.use(express.json());
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 
